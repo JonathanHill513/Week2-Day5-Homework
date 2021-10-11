e70eea673a984d4b6f6aa38bb04f06d7be6004b5
JonathanHill513 <jonnyhill513@gmail.com>
2021-10-11 00:28:54 -0400

    push

---
+++ test.js
@@ -0,0 +1,11 @@
+alert("BRUH!");
+var variable;
+variable =  10;
+
+var variable2 = "Jonathan";
+
+document.write(variable2);
+console.log(variable2);
+
+document.write(variable);
+console.log(variable);
